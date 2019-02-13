fb7a5534cd6285fe30bb2395adcc0b368d11a1d8
alexandre <alexandre.douchet.pro@gmail.com>
2019-02-13 16:45:14 +0100

    Alex

--- script.js
+++ script.js
@@ -14,11 +14,12 @@
         setTimeout(carousel, 4500);
     }
 
-    function printTime() {
-        var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hours: 'numeric', mins: "numeric"};
-        var d = new Date();
-        var date = d.toLocaleDateString('fr-FR', options);
-        var time = d.toLocaleTimeString('fr-FR');
-        document.getElementById("p").innerHTML = date + " - " + time;
-    }
-    setInterval(printTime, 1000);
+    // function printTime() {
+    //     var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hours: 'numeric', mins: "numeric"};
+    //     var d = new Date();
+    //     var date = d.toLocaleDateString('fr-FR', options);
+    //     var time = d.toLocaleTimeString('fr-FR');
+    //     document.getElementById("p").innerHTML = date + " - " + time;
+    // }
+    // setInterval(printTime, 1000);
+
