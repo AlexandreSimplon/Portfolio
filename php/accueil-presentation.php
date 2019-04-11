d19984ba225b1db0906cf7344d8e044f3317d522
alexandre <alexandre.douchet.pro@gmail.com>
2019-04-11 16:13:43 +0200

    Alex

--- php/accueil-presentation.php
+++ php/accueil-presentation.php
@@ -6,9 +6,10 @@
         <div class="row">
             <div class="col s6">
                 <!-- <p>&lt;html&gt;ceci est une balise&lt;/html&gt;</p> -->
-                <p>&lt;strong&gt;Message caché&lt;<span>/</span>strong&gt;</p>
-                <h6>&lt;h1>Salutation, noble surfer &lt;h1></h6>
-                <p>&lt;p>&lt;/p></p>
+                <p><<em>&excl;-- &lt;strong&gt;<strong>Message caché</strong>&lt;/strong&gt;</em></p>
+                <h6><em>&lt;h1>Salutation, noble surfer&lt;h1></em></h6>
+                <p><em>Je me présente, je me nomme <strong>&lt;strong>Alexandre&gt;/strong></strong>.<br>J’ai 33ans comme l’âge du Chris ;)<br>Ce message a pour but de vous plonger dans le code tout comme moi,<br>Je suis développeur WEB junior...<br>Si vous désirez plus d’informations sur moi ;<br>les infos sont en bas de page dans le &lt;footer>.</em></p>
+                <p><em>&lt;p><strong>Travail === Passion === Fun === Performance</strong>&lt;/p><em> --></em></p>
             </div>
         </div>
     </div>
