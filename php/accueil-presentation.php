d0364000d180af2f45b61cf6ac2fc629cc947bc1
alexandre <alexandre.douchet.pro@gmail.com>
2019-04-12 08:37:10 +0200

    alexandre

--- php/accueil-presentation.php
+++ php/accueil-presentation.php
@@ -4,12 +4,11 @@
 <div class="presentatonalex">
     <div class="container orange-text text-darken-4">
         <div class="row">
-            <div class="col xl4 l4 m6 s7">
-                <!-- <p>&lt;html&gt;ceci est une balise&lt;/html&gt;</p> -->
-                <p><<em>&excl;-- &lt;strong&gt;<strong>Message caché</strong>&lt;/strong&gt;</em></p>
+            <div class="col xl3 l4 m6 s7">
+                <p><<em>&excl;-- &lt;strong&gt;<strong>Message Caché</strong>&lt;/strong&gt;</em></p>
                 <h6><em>&lt;h1>Salutation, noble surfer&lt;h1></em></h6>
-                <p><em>Je me présente, je me nomme <strong>&lt;strong>Alexandre&gt;/strong></strong>.<br>J’ai 33ans comme l’âge du Chris ;)<br>Ce message a pour but de vous plonger dans le code tout comme moi,<br>Je suis développeur WEB junior...<br>Si vous désirez plus d’informations sur moi ;<br>les infos sont en bas de page dans le &lt;footer>.</em></p>
-                <p><em>&lt;p><strong>Travail === Passion === Fun === Performance</strong>&lt;/p><em> --></em></p>
+                <p><em>&lt;p>Je me présente, je me nomme <strong>&lt;strong>Alexandre&gt;/strong></strong>.<br>J’ai 33ans comme l’âge du Chris ;)<br>Ce message a pour but de vous plonger dans le code tout comme moi,<br>Je suis développeur WEB junior...<br>Si vous désirez plus d’informations sur moi ;<br>les infos sont en bas de page dans le &lt;footer>.&lt;p></em></p>
+                <p><em>&lt;p>&lt;strong><strong>Travail === Passion === Fun === Performance</strong>&lt;/strong>&lt;/p><em> --></em></p>
             </div>
         </div>
     </div>
