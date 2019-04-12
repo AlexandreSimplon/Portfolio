2288c54eca92ddff39a505af34232ccc280442a0
alexandre <alexandre.douchet.pro@gmail.com>
2019-04-12 11:47:23 +0200

    Alex

--- php/projets.php
+++ php/projets.php
@@ -1,3 +1,39 @@
 <div id="presentation" class="competences center blue darken-2">
     <h3 class="black-text accent-4 competences1">Projets</h3>
 </div>
+
+<div class="row">
+    <!-- <div class='col s12 m6 l6'> -->
+
+<?php
+$reponse = $bdd->query('SELECT * FROM projets');
+
+while ($donnees = $reponse->fetch())
+{
+
+echo"
+
+
+
+    <div class='card medium col s12 m6 l3 '>
+        <div class='card-image'>
+            <img src='".$donnees['img']."'>
+        </div>
+        <div class='card-content'>
+            <span class='card-title'>".$donnees['titre']."</span>
+            <p>".$donnees['description']."</p>
+        </div>
+        <div class='card-action'>
+            <a href='#'>Link</a>
+        </div>
+    </div>
+
+
+
+    ";}
+
+$reponse->closeCursor();
+?>
+        
+    <!-- </div> -->
+</div>
